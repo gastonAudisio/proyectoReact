f11bd36ed2cfaf8edd3df1d3ded4e753f1852d83
audisiogaston <audisiogaston@gmail.com>
2023-02-05 12:23:41 -0300

    vuelvo a dejarlo funcional

--- src/components/itemDetail/ItemDetail.jsx
+++ src/components/itemDetail/ItemDetail.jsx
@@ -1,11 +1,11 @@
 import ItemCount from "../itemCount/ItemCount";
 import { Link } from "react-router-dom";
 import { cartContext } from "../../storage/cartContext";
-import React, { useContext,useState } from "react";
+import React, { useContext } from "react";
 
 
 function ItemDetail({product}){
-  const { addItem , removeItem , clearCart } = useContext(cartContext);
+  const { addItem  } = useContext(cartContext);
   function handleAddToCart(count) {
     alert(`Agregaste ${count} de ${product.title} al carrito`);
     product.count = count;
@@ -14,8 +14,7 @@ function ItemDetail({product}){
   }
 
 return(
-<div>
-{isInCart ? (
+
   <div className="card-detail_main">
   <div className="card-detail_img">
     <img src={product.imgurl} alt={product.title} />
@@ -33,29 +32,9 @@ return(
     
   </div>    
 </div>
-) : (
-  <div className="card-detail_main">
-        <div className="card-detail_img">
-          <img src={product.imgurl} alt={product.title} />
-        </div>
-        <div className="card-detail_detail">
-            <h1>{product.title}</h1>
-            <h2 className="priceTag">$ {product.price}</h2>
-            <small>{product.detail}</small>
-      
-            <Link to="/cart">
-              <button className="btn">Ver Carrito</button> 
-            </Link>
-          
-        
-        </div>    
-  </div>
+
 )
 
 }
 
-</div>
-);
-
-}
 export default ItemDetail;
